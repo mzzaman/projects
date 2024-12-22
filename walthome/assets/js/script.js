c15d25f7844f3aa6dd4692820325746b4e4f157b
moniruzzaman <moniruzzamanm734@gmail.com>
2024-12-22 18:57:25 +0600

    Wealthome Project

---
+++ walthome/assets/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+const $header = document.querySelector("[data-header]");
+window.addEventListener("scroll", (e) => {
+  $header.classList[window.scrollY > 50 ? "add" : "active"]("active");
+});
+
+// Add to favorite button toggle;
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () =>
+    $toggleBtn.classList.toggle("active")
+  );
+});
