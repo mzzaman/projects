aaf7a4b074493b83ea1d030a191b3c4d1f2c0291
moniruzzaman <moniruzzamanm734@gmail.com>
2024-12-29 20:58:44 +0600

    Travelia Added

---
+++ Travelia/assets/js/script.js
@@ -0,0 +1,34 @@
+// Preloader Parts
+const preloader = document.querySelector("[data-preloader]");
+window.addEventListener("load", () => {
+  preloader.classList.add("remove");
+});
+
+// Add event on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// Navbar toggler for mobile
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+// Header
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.window.scrollY > 100 ? "add" : "remove"]("active");
+});
